924ec74559fa67f1e3d2cad1639846ddf3c7cd65
noriwaka <airsoft.manatsu@gmail.com>
2024-02-11 05:54:13 +0000

    add_follow_to_web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MicropostsController;
+use App\Http\controllers\UserFollowController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,6 +23,13 @@ Route::get('/dashboard', [MicropostsController::class, 'index'])->middleware(['a
 require __DIR__.'/auth.php';
 
 Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.fllow');
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow');
+        Route::get('followings', [UserController::class, 'followings'])->name('users.followings');
+        Route::get('followers', [UserController::class, 'followers'])->name('users.followers');
+    });
+    
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
