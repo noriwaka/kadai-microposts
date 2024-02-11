fb8c82c1d7daacc28a680cae0ace0c37a753bc5c
noriwaka <airsoft.manatsu@gmail.com>
2024-02-11 06:43:19 +0000

    add_follow_to_User_model

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     
     public function loadRelationshipCounts()
     {
-        $this->loadCount('microposts');
+        $this->loadCount(['microposts', 'follow', 'followers']);
     }
     
     
